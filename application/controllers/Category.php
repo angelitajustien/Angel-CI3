9b8b43de767c936b5c901ca8ea0b36d7e0bbe957
ANGEL-PC\Angel <angelita.lmj15@gmail.com>
2018-05-16 16:14:51 +0700

    interface register

--- application/controllers/Category.php
+++ application/controllers/Category.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Category extends CI_Controller{
 	public function __construct()
 	{
-		parent::__construct();
+		parent::__construct();/
 
 		// Load custom helper applications/helpers/MY_helper.php
 
---
+++ application/views/users/register.php
@@ -0,0 +1,26 @@
+<?php echo form_open('user/register', array('class' => 'needs-validation', 'novalidate' => '')); ?>
+ <main role="main" class="container">
+   <div class="form-group">
+       <label>Nama Lengkap</label>
+       <input type="text" class="form-control" name="nama" placeholder="Nama Lengkap">
+   </div>
+   <div class="form-group">
+       <label>Kodepos</label>
+       <input type="text" class="form-control" name="kodepos" placeholder="Kodepos">
+   </div>
+   <div class="form-group">
+       <label>Email</label>
+       <input type="text" class="form-control" name="email" placeholder="Email">
+   </div>
+   <div class="form-group">
+       <label>Username</label>
+       <input type="text" class="form-control" name="username" placeholder="Username">
+   </div>
+   <div class="form-group">
+       <label>Password</label>
+       <input type="text" class="form-control" name="password" placeholder="Password">
+   </div>
+
+   <button type="submit" class="btn btn-primary btn-block">Daftar</button>
+</main>
+<?php echo form_close(); ?>
